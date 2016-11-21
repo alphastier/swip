b338dcabb128adbf413505ec2ee78826b2c48e04
Hagglord <n.pound.euro@gmail.com>
2016-11-21 11:12:09 +0100

    in account.php links aktualisiert

--- account.php
+++ account.php
@@ -109,10 +109,10 @@
 			<!-- Main navigation -->
 			<ul id="headernavigation" class="nav navbar-nav">
 				<li class="active"><a href="#page-top">Events</a></li>
-                <li class="inactive"><a href="/favoriten.php">Favoriten</a></li>
-				<li class="inactive"><a href="#subscribe">FAQ</a></li>
-				<li class="inactive"><a href="#account">Account</a></li>
-                <li class="inactive"><a href="/index.php">Abmelden</a></li>
+                <li class="inactive"><a href="favoriten.php">Favoriten</a></li>
+				<!-- <li class="inactive"><a href="subscribe">FAQ</a></li> -->
+				<li class="inactive"><a href="account.php">Account</a></li>
+                <li class="inactive"><a href="index.php">Abmelden</a></li>
 			</ul> <!-- /.nav .navbar-nav -->
 		</nav> <!-- /.navbar-collapse  -->
 	</div><!-- /#main-menu -->
