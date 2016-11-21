5a171eed8b4fccc19f19e6e611318664b0a7b378
Hagglord <n.pound.euro@gmail.com>
2016-11-21 09:20:45 +0100

    account.php seite erstellt

---
+++ account.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>	Enjoy Switzerland </title>
+	<meta name="description" content="swip" >
+	<meta name="author" content="Jewel Theme">
+
+	<!-- Mobile Specific Meta -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+
+	<!-- Bootstrap  -->
+	<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- icon fonts font Awesome -->
+	<link href="assets/css/font-awesome.min.css" rel="stylesheet">
+
+	<!-- Custom Styles -->
+	<link href="assets/css/style.css" rel="stylesheet">
+
+	<!--[if lt IE 9]>
+	<script src="assets/js/html5shiv.js"></script>
+	<![endif]-->
+
+    <!-- jQuery -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js" type="text/javascript"></script>
+<script src="http://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=places"></script>
+
+</head>
+<body>
+
+
+	<!-- Preloader -->
+	<div id="preloader">
+		<div id="loader">
+			<div class="dot"></div>
+			<div class="dot"></div>
+			<div class="dot"></div>
+			<div class="dot"></div>
+			<div class="dot"></div>
+			<div class="dot"></div>
+			<div class="dot"></div>
+			<div class="dot"></div>
+			<div class="lading"></div>
+		</div>
+	</div><!-- /#preloader -->
+	<!-- Preloader End-->
+
+
+	<!-- Main Menu -->
+	<div id="main-menu" class="navbar navbar-default navbar-fixed-top" role="navigation">
+
+		<div class="navbar-header">
+			<!-- responsive navigation -->
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+				<span class="sr-only">Toggle navigation</span>
+				<i class="fa fa-bars"></i>
+			</button> <!-- /.navbar-toggle -->
+
+		</div> <!-- /.navbar-header -->
+
+		<nav class="collapse navbar-collapse">
+			<!-- Main navigation -->
+			<ul id="headernavigation" class="nav navbar-nav">
+				<li class="active"><a href="#page-top">Events</a></li>
+                <li class="inactive"><a href="/favoriten.php">Favoriten</a></li>
+				<li class="inactive"><a href="#subscribe">FAQ</a></li>
+				<li class="inactive"><a href="#account">Account</a></li>
+                <li class="inactive"><a href="/index.php">Abmelden</a></li>
+			</ul> <!-- /.nav .navbar-nav -->
+		</nav> <!-- /.navbar-collapse  -->
+	</div><!-- /#main-menu -->
+	<!-- Main Menu End -->
+
+
+	<!-- Page Top Login Section -->
+	<section id="page-top" class="section-style" data-background-image="images/background/image1.jpg">
+		<div class="pattern height-resize">
+			<div class="container">
+				<h1 class="site-title">
+					swip
+				</h1><!-- /.site-title -->
+				<h3 class="section-name">
+					<span>
+						Events
+					</span>
+				</h3><!-- /.section-name -->
+
+				<!-- Event List -->
+			 	<div class="panel-group" id="accordion">
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapse1">
+        Event 1</a>
+        <button type="button" class="btn-default .btn-m"><span>Fav</span></button>
+        <a class="event-date">23.12.16</a>
+        <a class="event-price">50 CHF</a>
+      </h4>
+    </div>
+    <div id="collapse1" class="panel-collapse collapse in">
+      <div class="panel-body">
+      	<p class="event-starttime">Beginnt um 18:30 Uhr.</p>
+        <p class="event-duration">Dauert 2 Stunden.</p>
+      	<p class="event-place"> Interlaken</p><p class="event-font">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.</p><br>
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">
+        Event 2</a>
+        <button type="button" class="btn-default .btn-m"><span>Fav</span></button>
+        <a class="event-date">23.12.16</a>
+        <a class="event-price">50 CHF</a>
+      </h4>
+    </div>
+    <div id="collapse2" class="panel-collapse collapse">
+      <div class="panel-body">
+      	<p class="event-starttime">Beginnt um 18:30 Uhr.</p>
+        <p class="event-duration">Dauert 2 Stunden.</p>
+      	<p class="event-place"> Interlaken</p><p class="event-font">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.</p><br>
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapse3">
+        Event 3</a>
+        <button type="button" class="btn-default .btn-m"><span>Fav</span></button>
+        <a class="event-date">23.12.16</a>
+        <a class="event-price">50 CHF</a>
+      </h4>
+    </div>
+    <div id="collapse3" class="panel-collapse collapse">
+      <div class="panel-body">
+      	<p class="event-starttime">Beginnt um 18:30 Uhr.</p>
+        <p class="event-duration">Dauert 2 Stunden.</p>
+      	<p class="event-place"> Interlaken</p><p class="event-font">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.</p><br>
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapse4">
+        Event 4</a>
+        <button type="button" class="btn-default .btn-m"><span>Fav</span></button>
+        <a class="event-date">23.12.16</a>
+        <a class="event-price">50 CHF</a>
+      </h4>
+    </div>
+    <div id="collapse4" class="panel-collapse collapse">
+      <div class="panel-body">
+      	<p class="event-starttime">Beginnt um 18:30 Uhr.</p>
+        <p class="event-duration">Dauert 2 Stunden.</p>
+      	<p class="event-place"> Interlaken</p><p class="event-font">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.</p><br>
+      </div>
+    </div>
+  </div><div class="panel panel-default">
+    <div class="panel-heading">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapse5">
+        Event 5</a>
+        <button type="button" class="btn-default .btn-m"><span>Fav</span></button>
+        <a class="event-date">23.12.16</a>
+        <a class="event-price">50 CHF</a>
+      </h4>
+    </div>
+    <div id="collapse5" class="panel-collapse collapse">
+      <div class="panel-body">
+      	<p class="event-starttime">Beginnt um 18:30 Uhr.</p>
+        <p class="event-duration">Dauert 2 Stunden.</p>
+      	<p class="event-place"> Interlaken</p><p class="event-font">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.</p><br>
+      </div>
+    </div>
+  </section>
+
+		<!-- Footer Section -->
+		<footer id="footer-section">
+			<p class="copyright">
+			By Miriam, Nora and Remo
+			</p>
+		</footer>
+		<!-- Footer Section End -->
+
+        <!-- WhatsNear Map -->
+
+
+
+
+		<!-- jQuery Library -->
+		<script type="text/javascript" src="assets/js/jquery-2.1.0.min.js"></script>
+		<!-- Modernizr js -->
+		<script type="text/javascript" src="assets/js/modernizr-2.8.0.min.js"></script>
+		<!-- Plugins -->
+		<script type="text/javascript" src="assets/js/plugins.js"></script>
+		<!-- Custom JavaScript Functions -->
+		<script type="text/javascript" src="assets/js/functions.js"></script>
+		<!-- Custom JavaScript Functions -->
+		<script type="text/javascript" src="assets/js/jquery.ajaxchimp.min.js"></script>
+
+	</body>
+	</html>
