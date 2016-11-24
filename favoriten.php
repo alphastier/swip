78223960e7a11fd8ac4441e241e95cd8c0d639a5
Miriam Meier <meier.miriam@hotmail.com>
2016-11-24 08:51:24 +0100

    favoriten.php Seite abgesichert, Rückverweis auf index.php

--- favoriten.php
+++ favoriten.php
@@ -1,3 +1,13 @@
+<?php
+  session_start();
+	if(!isset($_SESSION['user_id'])){
+		header("Location:index.php");
+	}else{
+  	$user_id = $_SESSION['user_id'];
+	}
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
